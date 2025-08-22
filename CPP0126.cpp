9f0496ad0ebc031e4ef1100f82dd353cef037ea3
anh-may2k6 <hoangquocanh2n6@gmail.com>
2025-08-22 19:39:07 +0700

    Update CPP0126.cpp

--- CPP0126.cpp
+++ CPP0126.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 #define Max 1000000
 
-char prime[Max+1];
+char prime[Max+1]; // cho biến theo kiểu char để dùng memset
 
 void sang(){
-    memset(prime,1,sizeof(prime));
-    prime[0] = 0,prime[1] = 0;
-    for(int i=2;i<=sqrt(Max);i++){
-        if(prime[i]){
-            for(int j=i*i;j<=Max;j+=i){
+    memset(prime,1,sizeof(prime)); // gán từ 0-10^6 là 1
+    prime[0] = 0,prime[1] = 0; // ko phải số nguyên tố thì là 0
+    for(int i=2;i<=sqrt(Max);i++){ // duyệt bằng cách này thì độ phức tạp chỉ là căn n
+        if(prime[i]){ // nếu là số nguyên tố thì bỏ qua giảm kha khá time
+            for(int j=i*i;j<=Max;j+=i){ // các biến có liên quan đến số ko phải số nguyên tố trong tương lai thì cho = 0
                 prime[j] = 0;
             }
         }
