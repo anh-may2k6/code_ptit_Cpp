9b83d8e10f1f86f58d68d2cd18d08796f0faf9ea
May <hoangquocanh2n6@gmail.com>
2025-08-22 19:35:03 +0700

    Liet ke so nguyen to -2

---
+++ CPP0126.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define Max 1000000
+
+char prime[Max+1];
+
+void sang(){
+    memset(prime,1,sizeof(prime));
+    prime[0] = 0,prime[1] = 0;
+    for(int i=2;i<=sqrt(Max);i++){
+        if(prime[i]){
+            for(int j=i*i;j<=Max;j+=i){
+                prime[j] = 0;
+            }
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    sang();
+    int t;
+    cin >> t;
+    while(t--){
+        int m,n;
+        cin >> m >> n;
+        for(int i=m;i<=n;i++){
+            if(prime[i]){
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
