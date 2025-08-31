c2dc52efb69a4fc6fee0d500580ee1b2f6e54bdd
May <hoangquocanh2n6@gmail.com>
2025-08-31 21:46:43 +0700

    Khao bao lop nhan vien

---
+++ CPP0606.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class NhanVien{
+private:
+    string name,gender,date,addr,id,time;
+public:
+    friend istream& operator>>(istream& in,NhanVien &a);
+    friend ostream& operator<<(ostream& out,const NhanVien &a);
+};
+
+istream& operator >> (istream& in,NhanVien &a){
+    getline(in,a.name);
+    in >> a.gender;
+    in.ignore();
+    getline(in,a.date);
+    getline(in,a.addr);
+    in >> a.id;
+    in.ignore();
+    getline(in,a.time);
+    return in;
+}
+
+ostream& operator << (ostream& out,const NhanVien &a){
+    out << setw(5) << setfill('0') << 1 << " " <<
+    a.name << " " << a.gender << " " << a.date <<
+    " " << a.addr << " " << a.id << " " << a.time;
+    return out;
+}
+
+int main(){
+    NhanVien a;
+    cin >> a;
+    cout << a;
+    return 0;
+}
