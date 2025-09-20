c66d2850bbf70d4c7d6e8f5dbcbcaa63e10d10cd
anh-may2k6 <hoangquocanh2n6@gmail.com>
2025-09-20 16:52:11 +0700

    Update CPP0525.cpp

--- CPP0525.cpp
+++ CPP0525.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
-
 using namespace std;
 
-struct SinhVien{
-    string id , name , lop;
-    double x,y,z;
+struct SinhVien {
+    string id, name, lop;
+    double x, y, z;
 
-    void nhap(){
-        cin >> id;
-        cin.ignore();
-        getline(cin , name);
-        cin >> lop;
+    void nhap() {
+        cin.ignore();         // bỏ xuống dòng sau khi đọc n
+        getline(cin, id);
+        getline(cin, name);
+        getline(cin, lop);
         cin >> x >> y >> z;
     }
 
-    void in(){
-        cout << id << " " << name << " " << lop <<
-        fixed << setprecision(1) << x << " " <<
-        y << " " << z << endl;
+    void in() {
+        cout << id << " " << name << " " << lop << " "
+             << fixed << setprecision(1) << x << " "
+             << y << " " << z << endl;
     }
 };
 
-bool cmp(SinhVien &a,SinhVien &b){
+bool cmp(SinhVien &a, SinhVien &b) {
     return a.name < b.name;
 }
 
-int main(){
+int main() {
     int n;
     cin >> n;
     SinhVien ds[n];
-    for(int i=0;i<n;i++){
-        ds[i].nhap();
-    }
+    for (int i = 0; i < n; i++) ds[i].nhap();
 
-    sort(ds,ds+n,cmp);
+    sort(ds, ds + n, cmp);
 
-    for(int i=0;i<n;i++){
-        cout << i+1 << " "; 
+    for (int i = 0; i < n; i++) {
+        cout << i + 1 << " ";
         ds[i].in();
     }
     return 0;
