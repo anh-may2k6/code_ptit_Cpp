1c2c2068a9b0411b5d563a75901bdfa61d12f7ea
anh-may2k6 <hoangquocanh2n6@gmail.com>
2025-09-29 23:46:00 +0700

    Update CPP0721.cpp

--- CPP0721.cpp
+++ CPP0721.cpp
@@ -1,3 +1,4 @@
+// C1: O(n^2) : QHD
 #include <iostream>
 #include <vector>
 
@@ -34,3 +35,39 @@ int main(){
     }
     return 0;
 }
+
+//C2: O(nlogn) QHD + Binary_search
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// void test_case(){
+//     int n;
+//     cin >> n;
+//     vector<int> a(n);
+//     for(int i=0;i<n;i++){
+//         cin >> a[i];
+//     }
+//     vector<int> dp; // tạo ra 1 mảng mới chỉ để lưu xâu con dài nhất
+//     for(int x : a){
+//         auto it = lower_bound(dp.begin(),dp.end(),x); // tìm vị trí mà phần tử đầu tiên >= x
+//         if(it == dp.end()) dp.push_back(x); // nếu không có thì thêm vào xâu dài nhất
+//         else *it = x; // thay đổi tại vị trí it gán bằng x 
+//         // vd: 2 4 10 6 
+//         // 2 4 10 -> 2 4 6 
+//     }
+//     cout << dp.size() << endl;
+// }
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int t;
+//     cin >> t;
+//     while(t--){
+//         test_case();
+//     }
+//     return 0;
+// }
