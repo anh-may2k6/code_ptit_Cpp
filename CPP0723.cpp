4a45a9c4415f389ef894e2bd12a89a5af7b55d07
May <hoangquocanh2n6@gmail.com>
2025-11-01 18:09:48 +0700

    Tich giai thua cac chu so

--- CPP0723.cpp
+++ CPP0723.cpp
@@ -1,37 +1,70 @@
-// với bài này thì ko nên chạy từ 1 cho đến y vì rất chậm và nếu y 10^18 thì có thể bị TLE
-// nên dùng lũy thừa nhị phân để giải quyết
-
+//bài này tốt nhất là đưa về giai thừa các số nguyên tố là hợp lý
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
-using ll = long long;
 
-ll luy_thua(ll x,ll y,ll p){
-    ll res = 1;
-    x%=p; // nếu x lớn hơn p thì lấy dư luôn
-    while(y > 0){
-        if(y & 1){ // nếu y lẻ thì phải cộng thêm phần dư lẻ ra 
-            res = (res * x) % p;
+void test_case() {
+    int n;
+    string s;
+    cin >> n >> s;
+    
+    vector<int> digits;
+    
+    // Phân tích từng chữ số
+    for(auto c : s){
+        int num = c - '0';
+        switch (num){
+            case 4: // 4! = 3! * 2! * 2!
+            digits.push_back(3);
+            digits.push_back(2);
+            digits.push_back(2);
+            break;
+            case 6: // 6! = 5! * 3!
+            digits.push_back(5);
+            digits.push_back(3);
+            break;
+            case 8: // 8! = 7! * 2! * 2! * 2!
+            digits.push_back(7);
+            digits.push_back(2);
+            digits.push_back(2);
+            digits.push_back(2);
+            break;
+            case 9: // 9! = 7! * 3! * 3! * 2!
+            digits.push_back(7);
+            digits.push_back(3);
+            digits.push_back(3);
+            digits.push_back(2);
+            break;
+            default:
+            // với số nguyên tố thì giữ lại
+            if(num > 1){
+                digits.push_back(num);
+            }
+            break;
         }
-        x = (x * x) % p; // mỗi vòng lặp *x cho đến khi y = 0
-        y >>= 1; // mỗi vòng lặp giảm y đi 1 nửa thì độ phức tạp chỉ còn là o(logy) nhanh hơn rất nhiều so với o(y)
     }
-    return res;
+    
+    // Sắp xếp giảm dần để được số lớn nhất
+    sort(digits.rbegin(), digits.rend());
+    
+    // In kết quả
+    for (int d : digits) {
+        cout << d;
+    }
+    cout << endl;
 }
 
-void test_case(){
-    ll x,y,p;
-    cin >> x >> y >> p;
-    cout << luy_thua(x,y,p) << endl;
-}
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         test_case();
     }
     return 0;
-}
+} 
