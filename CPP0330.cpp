8a4aa1c92a27cab3df639ab99a68af0dfd7ebbb7
May <hoangquocanh2n6@gmail.com>
2025-11-11 18:10:54 +0700

    Số đối xứng lớn nhất

--- CPP0330.cpp
+++ CPP0330.cpp
@@ -1,48 +1,85 @@
-#include<iostream>
-#include<string>
-#include<vector>
-using namespace std;
-int main() {
-	int t; cin >> t;
-	while (t--) {
-		string s,a; cin >> s;
-		vector<int> M(110, 0);
-		for (int i = 0; i < s.length(); i++)M[s[i]-'0']++;
-		int max = 10; int check = 0,check1=0;
-		for (int i = 0; i < 10; i++) {
-			if (M[i] >= M[max] && M[i] % 2 == 1)max = i;
-			if (M[i] > 0 && M[i] % 2 == 0&&i>0)check = 1;
-			if (i > 0 && M[i] != 0)check1 = 1;
-		}
-		if (check1 != 0) {
-			if (check == 0) {
-				int m = 1;
-				for (int i = 1; i < 10; i++)
-					if (M[i] > 0 && M[i] >= M[m]) {
-						m = i;
-					}
-				for (int i = 0; i < M[m]; i++)a.push_back(m + '0');
-				cout << a << endl;
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-			}
-			else {
-				for (int i = 0; i < 10; i++)
-					if (M[i] % 2 == 1 && M[i] < M[max] || M[i] == M[max] && i < max)
-						M[i] = 0;
-				for (int i = 9; i >= 0; i--)
-					if (i != max)
-						for (int j = 0; j < M[i] / 2; j++)a.push_back(i + '0');
-				for (int i = 0; i < M[max]; i++)a.push_back(max + '0');
-				for (int i = 0; i < 10; i++)
-					if (i != max)
-						for (int j = 0; j < M[i] / 2; j++)a.push_back(i + '0');
-				while (a[0] == '0')a.erase(0, 1);
-				while (a[a.length() - 1] == '0')a.erase(a.length() - 1, 1);
-				cout << a << endl;
-			}
+using namespace std;
+
+string build_from_freq(string s){
+	vector<int> freq(11,0);
+	for(char c : s){
+		freq[c-'0']++;
+	}
+	bool chan = false;
+	bool check = false;
+	int MAX = 10;
+	for(int i=0;i<10;i++){
+		if(freq[i] >= freq[MAX] && freq[i] % 2 == 1){
+			MAX = i;
 		}
-		else {
-			cout << endl;
+		if(freq[i] > 0 && freq[i] % 2 == 0 && i > 0){
+			chan = true;
+		}
+		if(i > 0 && freq[i] != 0){
+			check = true;
 		}
 	}
+	string result;
+	if(check){
+		if(!chan){
+			int m = 1;
+			for(int i=1;i<10;i++){
+				if(freq[i] > 0 && freq[i] >= freq[m]){
+					m = i;
+				}
+			}
+			for(int i=0;i<freq[m];i++){
+				result.push_back(m + '0');
+			}
+		}
+		else{
+			for(int i=0;i<10;i++){
+				if((freq[i] % 2 == 1 && freq[i] < freq[MAX]) || (freq[i] == freq[MAX] && i < MAX)){
+					freq[i] = 0;
+				}
+			}
+			for(int i=9;i>=0;i--){
+				if(i != MAX){
+					for(int j=0;j<freq[i]/2;j++){
+						result.push_back(i+'0');
+					}
+				}
+			}
+			for(int i=0;i<freq[MAX];i++){
+				result.push_back(MAX + '0');
+			}
+			for(int i=0;i<10;i++){
+				if(i != MAX){
+					for(int j=0;j<freq[i]/2;j++){
+						result.push_back(i+'0');
+					}
+				}
+			}
+			while(!result.empty() && result.front() == '0') result.erase(result.begin());
+			while(!result.empty() && result.back() == '0') result.pop_back();
+		}
+	}
+	return result;
+}
+
+void test_case(){
+	string s;
+	cin >> s;
+	cout << build_from_freq(s) << endl;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+	while(t--){
+		test_case();
+	}
+	return 0;
 }
