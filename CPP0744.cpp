bdd5f9c0a7ff26e61629e33a443895babe4ae595
May <hoangquocanh2n6@gmail.com>
2025-11-18 21:15:52 +0700

    Bang mau R - G - B

--- CPP0744.cpp
+++ CPP0744.cpp
@@ -7,14 +7,16 @@ using ll = long long;
 void test_case(){
     int n , r , b , g;
     cin >> n >> r >> b >> g;
+    //dùng quy hoạch động để tính giai thừa n
     vector<ll> gt(n+1);
     gt[0] = 1;
     for(int i=1;i<=n;i++){
         gt[i] = gt[i-1] * i;
     }
+
     int l = n - r - b - g;
     ll res = 0;
-    
+    //với mỗi trường hợp 0 - l thì ta có tổng các cách chọn để hoán vị
     for(int i=0;i<=l;i++){
         for(int j=0;j<=l-i;j++){
             int k = l - i - j;
