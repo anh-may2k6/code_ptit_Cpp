fb39359892a2eaeb1fc4108e5df5e4573b64435d
May <hoangquocanh2n6@gmail.com>
2025-11-18 21:12:12 +0700

    Bang mau R - G - B

---
+++ CPP0744.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+void test_case(){
+    int n , r , b , g;
+    cin >> n >> r >> b >> g;
+    vector<ll> gt(n+1);
+    gt[0] = 1;
+    for(int i=1;i<=n;i++){
+        gt[i] = gt[i-1] * i;
+    }
+    int l = n - r - b - g;
+    ll res = 0;
+    
+    for(int i=0;i<=l;i++){
+        for(int j=0;j<=l-i;j++){
+            int k = l - i - j;
+            res += (gt[n] / (gt[i + r] * gt[j + b] * gt[k + g]));
+        }
+    }
+    cout << res << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+        test_case();
+    }
+    return 0;
+}
