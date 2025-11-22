bb3e267ee232a984f275b0643938a4193550dd15
May <hoangquocanh2n6@gmail.com>
2025-11-22 18:28:47 +0700

    PTIT25R1C - Tổng Hiệu

--- OLP233.cpp
+++ OLP233.cpp
@@ -12,16 +12,19 @@ inline void nhap(vector<ll> &a , int n){
 }
 
 ll Long_sum(vector<ll> &a){
+    //vì lấy trị tuyệt đối nên ta sẽ sort lại
     sort(a.begin() , a.end());
     int n = a.size();
 
     vector<ll> prefix(n);
     prefix[0] = a[0];
-    
+
+    //dùng tổng prefix để làm vì O(n) sẽ nhanh hơn rất nhiều so với O(n^2)
     for(int i=1;i<n;i++) prefix[i] = prefix[i-1] + a[i];
 
     ll sum = 0;
     for(int i=1;i<n;i++){
+        //với mỗi a[i] được lặp lại thì ta sẽ có các hiệu tương ứng
         sum += a[i]*1LL*i - prefix[i-1];
     }
     return sum;
